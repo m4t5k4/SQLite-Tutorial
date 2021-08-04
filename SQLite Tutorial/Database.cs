7f3d43ee322b74b3d562f6ff79f7b0c3790b1ce9
Matthias Reynders <matthias.reynders3@gmail.com>
2021-08-04 18:05:29 +0200

    Add project files.

---
+++ SQLite Tutorial/Database.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLite_Tutorial
+{
+    public class Database
+    {
+        private readonly SQLiteAsyncConnection _db;
+
+        public Database(string dbPath)
+        {
+            _db = new SQLiteAsyncConnection(dbPath);
+            _db.CreateTableAsync<Person>().Wait();
+        }
+
+        public Task<List<Person>> GetPeopleAsync()
+        {
+            return _db.Table<Person>().ToListAsync();
+        }
+
+        public Task<int> SavePersonAsync(Person person)
+        {
+            return _db.InsertAsync(person);
+        }
+    }
+}
---
+++ SQLite Tutorial/Person.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLite_Tutorial
+{
+   public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Subscribed { get; set; }
+    }
+}
